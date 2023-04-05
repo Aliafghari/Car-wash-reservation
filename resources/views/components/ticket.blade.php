a622bd76bcdad29bcd4ce28d12a38b3e85507ec2
Aliafghari <123637062+Aliafghari@users.noreply.github.com>
2023-04-06 00:49:09 +0330

    hw19_part1

---
+++ resources/views/components/ticket.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <p class="text-bold text-blue-700 border p-2 m-3 rounded border-purple-400 inline-block">Ticket
+        #{{ $ticket->ticket_number }}
+    </p>
+    <p class="text-bold text-blue-700 border p-2 m-3 rounded border-purple-400 inline-block bg-success">Phone Number:
+        {{ $ticket->phone }}</p>
+    <p class="text-bold text-blue-700 border p-2 m-3 rounded border-purple-400 inline-block bg-success">Full Name:
+        {{ $ticket->full_name }}</p>
+    <p class="text-bold text-blue-700 border p-2 m-3 rounded border-purple-400 inline-block bg-success">Time of arrival:
+        {{ $ticket->time_of_arrival }}</p>
+    <p class="text-bold text-blue-700 border p-2 m-3 rounded border-purple-400 inline-block bg-danger text-dark">توجه:
+        لطفا شماره قبض را برای مراحل بعدی ذخیره کنید.</p>
+    {{ $slot }}
+
+    <div class="m-3 ">
+        <x-link href="/ticket/{{ $ticket->id }}/edit"
+            class="btn btn-outline-info text-dark rounded-lg px-3">
+            Edit
+        </x-link>
+    </div>
+    <div class="m-3">
+        <form action="/ticket/{{ $ticket->id }}" method="POST" class="inline">
+            @csrf
+            @method('DELETE')
+            <x-submit class="btn btn-outline-info text-dark rounded-lg px-3">
+                Delete
+            </x-submit>
+        </form>
+    </div>
---
+++ resources/views/ticket/create.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+    <title>Document</title>
+</head>
+
+<body>
+    <x-app.layout title="Create a ticket">
+        <form action="/ticket" method="POST">
+            @csrf
+            <div class="mb-3">
+                <x-input class="form-control" name="phone" label="Phone number" type="tel" />
+            </div>
+            <div class="mb-3">
+                <x-input class="form-control" name="full_name" label="Full name" type="text" />
+            </div>
+            <div class="mb-3">
+                <x-input class="form-control" name="time_of_arrival" label="Time of arrival" type="datetime-local" min="09:00:00" max="21:00:00" />
+            </div>
+            <x-submit class="btn btn-primary">Wash my car</x-submit>
+        </form>
+    </x-app.layout>
